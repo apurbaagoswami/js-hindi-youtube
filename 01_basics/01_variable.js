a23d84f1af3a6c015b95c819f84d6f97ba5da6db
Apurbaa Goswami <apurbaagoswami@gmail.com>
2025-06-02 12:09:46 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 14425
+let accountEmail = "apurbaa@tcs.com"
+var password = "12345"
+acountCity = "Balasore"
+ //accountId = 2 // not allowed
+
+ accountEmail = "mamam@tatasteel.com"
+ password = "20012005"
+ acountCity = "Baripada"
+
+ console.table([accountId,accountEmail,password,acountCity])
