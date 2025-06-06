8c454b24c61fa13212d108a7eb363ba620fd25d0
Apurbaa Goswami <apurbaagoswami@gmail.com>
2025-06-06 13:50:19 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+// let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// let abc = null
+// console.log(typeof abc);
+
+// let xyz = undefined
+// console.log(typeof xyz);
+
+/* 
+When we convert string to number:-
+"33" => 33
+"33abc" => NaN (not an number)
+ true => 1; false => 0
+ */
+
+//  let isLoggedIn = 0
+
+//  let booleanIsLoggedIn = Boolean(isLoggedIn) 
+//  console.log(booleanIsLoggedIn);
+
+ /* 
+ "apurbaa => true
+ 1 => true
+ 0 => false
+ */
+
+ let someNumber = 33
+
+ let stringnumber = String(someNumber)
+ console.log(stringnumber);
