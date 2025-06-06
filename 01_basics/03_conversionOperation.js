aab8d3fff2c332a4d242c891bdbe69b9dd5d1a40
Apurbaa Goswami <apurbaagoswami@gmail.com>
2025-06-06 14:19:03 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,7 +30,38 @@ When we convert string to number:-
  0 => false
  */
 
- let someNumber = 33
+//  let someNumber = 33
 
- let stringnumber = String(someNumber)
- console.log(stringnumber);
+//  let stringnumber = String(someNumber)
+//  console.log(stringnumber);
+
+// *****************************  OPERATIONS  *********************************
+
+// let value = 3
+// let negValue= -value 
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);//power **
+// console.log(2/2);
+// console.log(2%2);// remainder
+
+// let str1 = "Apurbaa"
+// let str2 = " Goswami"
+// let str3 = str1 + str2;
+// console.log(str3);
+
+console.log(+true) //gives 1
+console.log(+""); //gives 0
+
+let num1 = num2 = num3 = 2 + 2 // this is not good no readibility 
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+let gameCounter1= 100
+++gameCounter;
+console.log(gameCounter1 );
