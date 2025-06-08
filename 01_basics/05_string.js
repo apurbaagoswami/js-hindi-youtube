d62ad988c3dac25730e0cf47575d28345832922a
Apurbaa Goswami <apurbaagoswami@gmail.com>
2025-06-08 11:37:34 +0000

    Strings 1

---
+++ 01_basics/05_string.js
@@ -0,0 +1,21 @@
+const name = "Apurbaa"
+const repoCount =  2
+
+// console.log( name + repoCount + " value"); // adding or concatinating
+
+//but nowadays we use backticks
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+//another way to declare string
+const gameName = new String('Apurbaahc')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+console.log(gameName.indexOf("b"));
+// Sliicng
+const newString = gameName.substring(0 , 4)
+console.log(newString);
+
