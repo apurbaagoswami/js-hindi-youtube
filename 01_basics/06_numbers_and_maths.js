b5455b646bb556c15c8b39fdad88bd6632ae6a2e
Apurbaa Goswami <apurbaagoswami@gmail.com>
2025-06-08 14:43:50 +0000

    Numbers

---
+++ 01_basics/06_numbers_and_maths.js
@@ -0,0 +1,8 @@
+const score = 400
+console.log(score);
+//new is a key word
+const balance = new Number (100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2)); //to get precise data in e-commerce websites.
