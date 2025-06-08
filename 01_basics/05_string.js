4364d5a89dc8a20e64032dc193f9e508c78af4ee
Apurbaa Goswami <apurbaagoswami@gmail.com>
2025-06-08 14:30:45 +0000

    string 2

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -7,7 +7,7 @@ const repoCount =  2
 // console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 //another way to declare string
-const gameName = new String('Apurbaahc')
+const gameName = new String('Apurbaa-hc')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -15,7 +15,29 @@ const gameName = new String('Apurbaahc')
 // console.log(gameName.toUpperCase());
 // console.log(gameName.charAt(2));
 console.log(gameName.indexOf("b"));
-// Sliicng
+// methords ;-
+
+//SUBSTRING (we cannot give negative values there)
 const newString = gameName.substring(0 , 4)
 console.log(newString);
 
+//SLICING ( we can do negative values here)
+const anotherString = gameName.slice(-8 , 4) //going from backwaRD
+console.log(anotherString);
+
+//TRIM (sometimes when we give any online form the user enter so many unnecessary spaces which we dont wanr to store) 
+// used in white space characters
+const newStringOne = "   apurbaa@gmail.com         "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+// REPLACE 
+const url = "https://apurbaa.com/apurbaa%20goswami"
+
+console.log(url.replace('%20' , '-'));
+
+//INCLUDES (to ask if the keyword is present in the string,  used in social media to remove spam comments)
+console.log(url.includes('apurbaa'));
+
+//SPLIT 
+console.log(gameName.split('-'));
