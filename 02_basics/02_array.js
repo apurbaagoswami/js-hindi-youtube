81e7141d7a95186f16799fde2eb19e6a17be677c
Apurbaa Goswami <apurbaagoswami@gmail.com>
2025-06-13 06:12:47 +0000

    array functions

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -12,5 +12,23 @@ const dc_heros = ["Superman", "Flash", "Batman"]
 
 //using spread instead of concat 
 const all_new_heros = [...marvel_heros, ...dc_heros]
-console.log(all_new_heros);
+// console.log(all_new_heros);
 
+
+//FLAT : retuens a new array with all sub array elements concatinates into it.
+
+const another_array = [1, 2, 3, [4, 5, 6] , 7 , [6, 7,[1,9]]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+// ask or convert to an array
+
+console.log(Array.isArray("Apurbaa"));
+console.log(Array.from("Apurbaa")); //converts to an array
+console.log(Array.from({name : "Apurbaa"}));// Interview = actually it gives an empty array[]
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));//returns a new array from set of elements
