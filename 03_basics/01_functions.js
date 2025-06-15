bb964621af4efab8d938ee9d3179091e926b5581
Apurbaa Goswami <apurbaagoswami@gmail.com>
2025-06-15 14:34:00 +0000

    function 1

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,16 @@
+// node 03_basics/01_functions.js
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+// sayMyName() //execution (we dont use console.log here to print the output)
+// sayMyName //reference
+
+function addTwoNumbers(){
+    
+}
