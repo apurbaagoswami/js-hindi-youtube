11f57034a4602f30fd3a44c894a0e52e2922c1e8
Apurbaa Goswami <apurbaagoswami@gmail.com>
2025-06-21 13:44:55 +0000

    basic IF ELSE

--- 04_Control_flow/one.js
+++ 04_Control_flow/one.js
@@ -1,7 +1,47 @@
+// node 04_Control_flow/one.js
 //IF Statement
 
-const isUserLoggedIn = true
+// const isUserLoggedIn = true
+// const temperature = 41
 
-if (isUserLoggedIn) {
+// if ( temperature < 50) {
+//     console.log("Temp is less than 50");
+// }
+// // === and !== checks the datatype too strictly.
 
+//  if ( 2 === "2") {
+//     console.log("Executed");
+// } else {
+//     console.log("Not executed")
+// }
+
+// const score = 200
+
+// if (score > 100){
+//     const power = "fly"
+//     console.log(`User power &{power}`);
+// }
+
+// const balance = 1000
+
+// // if (balance > 500) console.log("Test"); //implicit scope(short hand)
+
+// if ( balance < 500) {
+//     console.log("less than");
+
+// } else if ( balance < 750 ) {
+//     console.log("less than 750");
+
+// }else if (balance < 900) {
+//     console.log("less than 900");
+// }
+//  else {
+//     console.log("Balance is less than 1200")
+// }
+
+const userLoggedIn = true
+const debitCard = true
+
+if ( userLoggedIn && debitCard) {
+    console.log("Allow to buy course")
 }
