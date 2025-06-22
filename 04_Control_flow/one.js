9f1c0cbd636293cdd9d4250e20698c9dc3b30f00
Apurbaa Goswami <apurbaagoswami@gmail.com>
2025-06-22 12:22:31 +0000

    one complete

--- 04_Control_flow/one.js
+++ 04_Control_flow/one.js
@@ -41,7 +41,13 @@
 
 const userLoggedIn = true
 const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
 
 if ( userLoggedIn && debitCard) {
     console.log("Allow to buy course")
+}
+
+if ( loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in.")
 }
