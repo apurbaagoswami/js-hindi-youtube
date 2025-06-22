1978ff61a4e72b09c2fd9eb03a6c5b263011455d
Apurbaa Goswami <apurbaagoswami@gmail.com>
2025-06-22 13:11:21 +0000

    truthy values

---
+++ 04_Control_flow/truthy.js
@@ -0,0 +1,16 @@
+// node 04_Control_flow/truthy.js
+
+const userEmail = "h@hitesh.ai" 
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("DOnt have user email");
+}
+
+//  FALSY VALUE  :-
+// false , 0 , -0 , BigInt 0n , "" , null , undefined , NaN
+
+// TRUTHY VALUES :-
+// true , "0" , 'false' , [] , () , {} , " ", function(){}
+
