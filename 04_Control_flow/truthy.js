55c49d1da9030d17797df011da57f8a652cb3b42
Apurbaa Goswami <apurbaagoswami@gmail.com>
2025-07-05 14:23:16 +0000

    truthy 2

--- 04_Control_flow/truthy.js
+++ 04_Control_flow/truthy.js
@@ -1,6 +1,6 @@
 // node 04_Control_flow/truthy.js
 
-const userEmail = "h@hitesh.ai" 
+const userEmail = [] 
 
 if (userEmail) {
     console.log("Got user email");
@@ -14,3 +14,33 @@ if (userEmail) {
 // TRUTHY VALUES :-
 // true , "0" , 'false' , [] , () , {} , " ", function(){}
 
+//checking the array if is empty
+
+if (userEmail.length === 0 ) {
+    console.log("Array is empty");
+    
+}
+
+// check is a object is empty
+
+const emoObj = {}
+
+if (Object.keys(emoObj).length === 0 ){
+    console.log("Object is empty");
+}
+
+// Nullish  Coalescing Operator (??): null undefined
+
+let val1 ;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+val1 = undefined ?? 15
+
+console.log(val1); //5
+
+//Ternary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("Less than 80") : console.log("More than 80");
